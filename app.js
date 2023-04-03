b15ad359313ec9ed387d49bb00c3ba6b1d260731
Udotter <129746704+Udotter@users.noreply.github.com>
2023-04-03 22:53:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
-// フォローボタンをクリックしたときの処理
-function followButtonClicked() {
-  // フォローボタンの要素を取得
-  const followButton = document.querySelector('#follow-button');
+// フォローボタン
+const followButton = document.querySelector('.follow-button');
+let isFollowing = false;
 
-  // フォローボタンの表示テキストを変更
-  followButton.textContent = 'フォロー中';
-
-  // フォロー数の要素を取得
-  const followerCount = document.querySelector('#follower-count');
-
-  // フォロー数を1増やす
-  followerCount.textContent = parseInt(followerCount.textContent) + 1;
-
-  // フォロー状態をサーバーに送信する処理を実行
-  sendFollowStatus(true);
-}
-
-// ツイートボタンをクリックしたときの処理
-function tweetButtonClicked() {
-  // ツイート内容を取得
-  const tweetText = document.querySelector('#tweet-text').value;
-
-  // ツイート内容が空の場合は何もしない
-  if (tweetText.trim() === '') {
-    return;
+followButton.addEventListener('click', () => {
+  if (isFollowing) {
+    followButton.innerText = 'フォローする';
+    followButton.style.backgroundColor = '#1DA1F2';
+    followButton.style.color = 'white';
+  } else {
+    followButton.innerText = 'フォロー中';
+    followButton.style.backgroundColor = '#E8F5FE';
+    followButton.style.color = '#1DA1F2';
   }
+  isFollowing = !isFollowing;
+});
 
-  // ツイートをサーバーに送信する処理を実行
-  sendTweet(tweetText);
+// ツイートボタン
+const tweetButton = document.querySelector('.tweet-button');
+tweetButton.addEventListener('click', () => {
+  // ツイートを投稿する処理
+});
 
-  // ツイートフォームをリセット
-  document.querySelector('#tweet-form').reset();
+// リツイートボタン
+const retweetButton = document.querySelector('.retweet-button');
+let isRetweeted = false;
 
-  // ツイート成功メッセージを表示
-  alert('ツイートしました！');
-}
+retweetButton.addEventListener('click', () => {
+  if (isRetweeted) {
+    retweetButton.innerText = 'リツイート';
+    retweetButton.style.backgroundColor = 'white';
+    retweetButton.style.color = '#1DA1F2';
+    retweetButton.style.borderColor = '#1DA1F2';
+  } else {
+    retweetButton.innerText = 'リツイート済み';
+    retweetButton.style.backgroundColor = '#E8F5FE';
+    retweetButton.style.color = '#1DA1F2';
+    retweetButton.style.borderColor = '#1DA1F2';
+  }
+  isRetweeted = !isRetweeted;
+});
+
+// お気に入りボタン
+const favoriteButton = document.querySelector('.favorite-button');
+let isFavorited = false;
+
+favoriteButton.addEventListener('click', () => {
+  if (isFavorited) {
+    favoriteButton.innerText = 'お気に入り';
+    favoriteButton.style.backgroundColor = 'white';
+    favoriteButton.style.color = '#1DA1F2';
+    favoriteButton.style.borderColor = '#1DA1F2';
+  } else {
+    favoriteButton.innerText = 'お気に入り済み';
+    favoriteButton.style.backgroundColor = '#E8F5FE';
+    favoriteButton.style.color = '#1DA1F2';
+    favoriteButton.style.borderColor = '#1DA1F2';
+  }
+  isFavorited = !isFavorited;
+});
