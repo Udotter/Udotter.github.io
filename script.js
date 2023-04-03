b1b5c871b52f1b65e2306a44d4db0a8820e32e88
Udotter <129746704+Udotter@users.noreply.github.com>
2023-04-03 22:15:05 +0900

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const likeBtns = document.querySelectorAll('.like-btn');
+const retweetBtns = document.querySelectorAll('.retweet-btn');
+const followBtns = document.querySelectorAll('.follow-btn');
+
+likeBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		btn.classList.toggle('liked');
+	});
+});
+
+retweetBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		alert('この投稿をリツイートしました！');
+	});
+});
+
+followBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		alert('このユーザーをフォローしました！');
+	});
+});
